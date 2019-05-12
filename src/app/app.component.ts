88a7a57ba65844ae9c5297f013baae4b228fb634
msantrax <msantrax@gmail.com>
2019-05-12 14:19:01 -0300

    Implementação do Bokeh - fase1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+
   title = 'NGGraph';
+
+  constructor() {
+
+
+  }
+
+
+  ngOnInit() {
+
+  }
+  
 }
+
+
+
