858b8b9b9d9ef58c999dfecb3a53e78221e5895c
msantrax <msantrax@gmail.com>
2019-06-05 18:33:51 -0300

    Trabalhando na implementação do proxy com o cloud functions -> instalando o request-promise

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,135 @@
-const functions = require('firebase-functions');
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// [START functionsimport]
+const functions = require('firebase-functions');
+// [END functionsimport]
+// [START additionalimports]
+// Moments library to format dates.
+const moment = require('moment');
+// CORS Express middleware to enable CORS Requests.
+const cors = require('cors')({
+  origin: true,
+});
+
+// According to firebase setup
+var admin = require("firebase-admin");
+var serviceAccount = require("../fbadmkey.json");
+// [END additionalimports]
+
+
+const requestjs = require('request');
+
+
+
+//Firebase adm setup
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sorptionlab.firebaseio.com"
+});
+
+
+
+// [START all]
+/**
+ * Returns the server's date. You must provide a `format` URL query parameter or `format` vaue in
+ * the request body with which we'll try to format the date.
+ *
+ * Format must follow the Node moment library. See: http://momentjs.com/
+ *
+ * Example format: "MMMM Do YYYY, h:mm:ss a".
+ * Example request using URL query parameters:
+ *   https://us-central1-<project-id>.cloudfunctions.net/date?format=MMMM%20Do%20YYYY%2C%20h%3Amm%3Ass%20a
+ * Example request using request body with cURL:
+ *   curl -H 'Content-Type: application/json' /
+ *        -d '{"format": "MMMM Do YYYY, h:mm:ss a"}' /
+ *        https://us-central1-<project-id>.cloudfunctions.net/date
+ *
+ * This endpoint supports CORS.
+ */
+// [START trigger]
+exports.date = functions.https.onRequest((req, res) => {
+  // [END trigger]
+  // [START sendError]
+  // Forbidding PUT requests.
+  if (req.method === 'PUT') {
+    return res.status(403).send('Forbidden!');
+  }
+  // [END sendError]
+
+  // [START usingMiddleware]
+  // Enable CORS using the `cors` express middleware.
+  return cors(req, res, () => {
+    // [END usingMiddleware]
+    // Reading date format from URL query parameter.
+    // [START readQueryParam]
+    let format = req.query.format;
+    // [END readQueryParam]
+    // Reading date format from request body query parameter
+    if (!format) {
+      // [START readBodyParam]
+      format = req.body.format;
+      // [END readBodyParam]
+    }
+    // [START sendResponse]
+    const formattedDate = moment().format(format);
+    console.log('Sending Formatted date:', formattedDate);
+    res.status(200).send(formattedDate);
+    // [END sendResponse]
+  });
+});
+
+
+exports.serverRequest = functions.https.onCall((request, response) => {
+
+  console.log(request.id);
+  const formattedDate = moment();
+  return 'Server returned : ' + formattedDate;
+
+});
+
+
+exports.serverProxy = functions.https.onCall((request, response) => {
+
+  var lbody = "lbody empty";
+
+  return requestjs('http://localhost:8080/artifacts1.json').then (() => {
+      console.log ("then called...");
+      return lbody;
+    }
+  );
+
+  return 'Return Imediatelly...';
+
+});
+
+
+
+
+// return requestjs('http://localhost:8080/artifacts1.json', function (error, response, body) {
+//     console.error('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     console.log('body loaded:'); // Print the HTML for the Google homepage.
+//     lbody = body;
+//   }).then (() => {
+//       console.log ("then called...");
+//       return lbody;
+//     }
+//   );
+
+
+// [END all]
